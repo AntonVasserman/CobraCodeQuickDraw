01cd55986eabe0ec6cc96473dfe18d29aa7be5db
Anton Vasserman <Anton.Vasserman@outlook.com>
2024-10-21 15:16:05 +0300

    Pushed rest of 26 & 28 changes

---
+++ Source/CobraCodeQuickDraw/Core/GameModes/QDGameModeBase.h
@@ -0,0 +1,14 @@
+// Copyright Anton Vasserman, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QDGameModeBase.generated.h"
+
+UCLASS()
+class COBRACODEQUICKDRAW_API AQDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
